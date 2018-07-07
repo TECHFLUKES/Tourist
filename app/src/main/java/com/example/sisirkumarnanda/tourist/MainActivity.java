7b2354f7ccf927bd9769070a15966a12ea4302e1
aakash1999 <aakash.nanda99@gmail.com>
2018-07-07 20:54:43 +0530

    Latest commit

--- app/src/main/java/com/example/sisirkumarnanda/tourist/MainActivity.java
+++ app/src/main/java/com/example/sisirkumarnanda/tourist/MainActivity.java
@@ -1,47 +1,128 @@
 package com.example.sisirkumarnanda.tourist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.sisirkumarnanda.tourist.Adapters.RecyclerViewAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
+import de.hdodenhof.circleimageview.CircleImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
 
-    private RecyclerView recyclerView;
-    private ArrayList<String> items;
+{
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private RecyclerView postList;
+    private Toolbar mToolbar;
+    private CircleImageView NavProfileImage;
+    private TextView NavProfileUserName;
+
+    String currentUserID;
+
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+          super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //resturants hotels schools hospitals market
-//        recyclerView = (RecyclerView)findViewById(R.id.myRecyclerView);
-//        items = new ArrayList<String>();
-//
-//        items.add("Market Place");
-//        items.add("Hospitals");
-//        items.add("Hotels");
-//        items.add("Schools");
-//        items.add("Restaurants");
-
-//        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(this,items);
-//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-//        recyclerView.setLayoutManager(layoutManager);
-//        recyclerView.setItemAnimator(new DefaultItemAnimator());
-//        recyclerView.setAdapter(recyclerViewAdapter);
 
 
+
+        mToolbar = (Toolbar) findViewById(R.id.main_app_bar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("TOURISM RAJASTHAN");
+
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
+        actionBarDrawerToggle =new ActionBarDrawerToggle(MainActivity.this,drawerLayout,R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
+        NavProfileImage = (CircleImageView) navView.findViewById(R.id.nav_profile_image);
+        NavProfileUserName = (TextView) navView.findViewById(R.id.nav_user_full_name);
+
+
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
+                UserMenuSelector(item);
+                return false;
+            }
+        });
 
 
 
 
 
     }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void UserMenuSelector(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.nav_home:
+
+                break;
+
+            case R.id.nav_needhelp:
+
+                break;
+
+            case R.id.nav_events:
+
+                break;
+
+            case R.id.nav_guide:
+
+                break;
+
+            case R.id.nav_setting:
+
+                break;
+            case R.id.nav_logout:
+
+                break;
+
+        }
+
+    }
+
+
 }
